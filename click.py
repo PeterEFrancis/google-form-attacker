400828fea142064d16ff94bc14616b7c8e2a8790
Peter Francis <francisp336@gmail.com>
2019-08-16 23:12:41 -0400

    load page edits

--- click.py
+++ click.py
@@ -2,80 +2,75 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
 
 import time
 
-print('\n\n  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n\n________ GOOGLE FORM ATTACKER ________\n\n')
+print('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('\n\n                    ________ GOOGLE FORM ATTACKER ________\n\n')
 
 # open log.txt and get total
 f = open('log.txt')
 total = int(f.readlines()[0])
 f.close()
 
-print('Loaded previous attacks. \n\n')
+print(f'Loaded {total} previous attacks. \n\n')
 
 # set number of attacks and URL
-attacks = int(input("Input the number of submissions you would like to attempt: "))
+attacks = int(input("Input the number of submissions you would like to attempt: \n >>> "))
 goal = attacks + total
 attempt = 0
-base = input('Input the base URL: ')
-if base[0:30] != 'https://docs.google.com/forms/':
-    print('\nNot a valid URL.')
-    base = input('Input the base URL: ')
+URL = input('\nInput the form URL: \n >>> ')
+while URL[0:30] != 'https://docs.google.com/forms/':
+    print('Not a valid URL.')
+    URL = input('Input the form URL: \n >>> ')
 
-form = base + '/viewform'
-response = base + '/formResponse'
+print('\n\nLoading the Web Driver...\n\n')
 
-# Open the driver and webpage
-browser = webdriver.Safari()
-# wait = WebDriverWait(browser, 10)
+# Open the web driver
+driver = webdriver.Safari()
 
-# Display that attacks have started and which have happened
-print(f"\n \n Attacks have started. \n \n")
-
-time.sleep(1)
+# Display that attacks have started
+print(f"Attacks have started.\n\n")
 
 while total < goal:
-    try:
-        browser.get(form)
+    # open the form
+    driver.get(URL)
 
-        # Click the right selection
-        browser.find_elements_by_class_name('quantumWizTogglePaperradioOffRadio')[0].click()
+    # wait for element to load
+    # try:
+    #     element_present = EC.presence_of_element_located((By.CLASS_NAME, 'quantumWizTogglePaperradioOffRadio'))
+    #     WebDriverWait(driver, 10).until(element_present)
+    # except selenium.common.exceptions.TimeoutException:
+    #     continue
+    time.sleep(1)
 
-        # Click the submit
-        browser.find_element_by_class_name('quantumWizButtonPaperbuttonFlat').click()
+    # Click the first selection
+    driver.find_elements_by_class_name('quantumWizTogglePaperradioOffRadio')[0].click()
 
-        # wait for redirect
-        # time.sleep(1)
-        # wait.until(EC.url_to_be(response))
+    # wait for element to load
+    # try:
+    #     element_present = EC.presence_of_element_located((By.CLASS_NAME, 'quantumWizButtonPaperbuttonFlat'))
+    #     WebDriverWait(driver, 10).until(element_present)
+    # except selenium.common.exceptions.TimeoutException:
+    #     continue
+    time.sleep(1)
 
-        # Click 'submit another'
-        # browser.find_elements_by_tag_name('a')[0].click()
+    # Click the submit
+    driver.find_element_by_class_name('quantumWizButtonPaperbuttonFlat').click()
 
-        # wait for redirect
-        # wait.until(EC.url_to_be(form))
+    # increment, feedback, and write to log
+    total += 1
+    print(f'Completed: {total} ({attempt+1}/{attacks})', end="\r")
 
-
-
-        # increment, feedback, and write to log
-        # total += 1
-        # print(f'Completed: {total} ({attempt+1}/{attacks})', end="\r")
-        #
-        # # update log.txt
-        # with open('log.txt', 'w') as f:
-        #     f.write(str(total))
-        # f.close()
-        # attempt += 1
-    except selenium.common.exceptions.TimeoutException:
-        browser.close()
-        # Open the driver and webpage
-        browser = webdriver.Safari()
-        browser.get(form)
-        wait = WebDriverWait(browser, 10)
+    # update log.txt
+    with open('log.txt', 'w') as f:
+        f.write(str(total))
+    f.close()
+    attempt += 1
 
 
+print(f'\n\n{attacks} Attacks have finished successfully!')
 
-
-print(f'\n\n {attacks} Attacks have finished successfully!')
-
-browser.close()
+driver.close()
